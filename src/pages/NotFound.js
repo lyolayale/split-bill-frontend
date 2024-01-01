b9ba4235571f756400d1b1c535dd4cd06555dd27
Eric McKee <lyolayale19@gmail.com>
2023-12-31 18:31:40 -0600

    Added content/styling to NotFound component.

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,17 @@
 export default function NotFound() {
   return (
-    <section className="h-screen">
-      <h1>404 Error Page Not Found</h1>
+    <section className=" h-screen w-max-screen-lg bg-slate-900 flex flex-col justify-center items-center gap-20">
+      <div className="relative p-tags mx-auto flex flex-column justify-center items-center">
+        <p className="mt-[-10rem] text-9xl font-extrabold text-stone-300">
+          404
+        </p>
+        <p className="absolute bg-orange-600 w-36 text-center uppercase bottom-[70px] rotate-12 text-sm">
+          page not found
+        </p>
+      </div>
+      <button className="w-36 p-2 text-orange-600 uppercase bg-transparent border border-orange-600 shadow-lg shadow-orange-600 hover:shadow-none duration-200">
+        Go Home
+      </button>
     </section>
   );
 }
