97006229a7575fcb369dc0b2ff94628f7dc1d934
Eric McKee <lyolayale19@gmail.com>
2024-01-07 02:49:39 -0600

    Styled FriendsList, Header & Footer -> shadows.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
 export default function Footer() {
   return (
     <footer className="fixed bottom-0 w-full">
-      <nav className="p-4 bg-gray-300 font-light text-xs">
-        <ul className="flex justify-center items-center gap-2">
+      <nav className="p-4 bg-gray-950 font-light text-xs">
+        <ul className="flex justify-center items-center gap-2 text-blue-700">
           <li>
             <p>&copy;Bill Splitter</p>
           </li>
-          <li className="font-hero relative">
+          <li className="font-hero text-green-700">
             <p>{new Date().getFullYear()}</p>
           </li>
         </ul>
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,15 @@ import { FaMoneyBillAlt } from "react-icons/fa";
 export default function Header() {
   return (
     <header>
-      <nav className="p-4 bg-gray-300">
+      <nav className="p-4 bg-gray-900">
         <ul className="flex justify-start items-center gap-2">
           <li>
             <PiScrollFill className="text-blue-800" size={40} />
           </li>
-          <li className="font-hero relative text-blue-800 font-extrabold">
+          <li className="font-hero relative text-blue-700 font-extrabold">
             Bill Splitter{" "}
             <span>
-              <FaMoneyBillAlt className="inline absolute top-0 text-green-800" />
+              <FaMoneyBillAlt className="inline absolute top-0 text-green-700" />
             </span>
           </li>
         </ul>
