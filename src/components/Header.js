13c02c3411a2f585ceee7f5d98d350b4f62f5ba7
Eric McKee <lyolayale19@gmail.com>
2024-01-27 16:23:00 -0600

    Added link to header to navigative to home page.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,25 @@
 import { PiScrollFill } from "react-icons/pi";
 import { FaMoneyBillAlt } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <header>
       <nav className="p-4 bg-gray-900">
         <ul className="flex justify-start items-center gap-2">
-          <li>
-            <PiScrollFill className="text-blue-800" size={40} />
-          </li>
-          <li className="font-hero relative text-blue-700 font-extrabold">
-            Bill Splitter{" "}
-            <span>
-              <FaMoneyBillAlt className="inline absolute top-0 text-green-700" />
-            </span>
-          </li>
+          <NavLink to="/split-bill-frontend">
+            <li>
+              <PiScrollFill className="text-blue-800" size={40} />
+            </li>
+          </NavLink>
+          <NavLink to="/split-bill-frontend">
+            <li className="font-hero relative text-blue-700 font-extrabold">
+              Bill Splitter{" "}
+              <span>
+                <FaMoneyBillAlt className="inline absolute top-0 text-green-700" />
+              </span>
+            </li>
+          </NavLink>
         </ul>
       </nav>
     </header>
